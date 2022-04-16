1a8dc4cf5d140db2f359c3ee70f3cb6dbaed6d83
Josh Klein <kleinjjosh74@gmail.com>
2022-04-16 17:31:53 -0400

    added questions to question array through inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,56 @@
 // TODO: Include packages needed for this application
 const inquirer = require(`inquirer`);
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: `What is your Github user name?`,
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your E-mail address?',
+        },
+        {
+            type: 'input',
+            name: 'project',
+            message: `What is the name of the project we are creating a README for?`,
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: `Please enter a description of the project.`,
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Which license do you need to include?',
+            choices: ['MIT', 'APACHE', 'GPL', 'BSD', 'None'],
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'What intalling commands are required for your project?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What usage instructions would you like to add?',
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Please add any contribution guidelines neccessary.',
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Please add any neccessary testing instructions.',
+        },
+    ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -10,4 +59,5 @@ function writeToFile(fileName, data) {}
 function init() {}
 
 // Function call to initialize app
-init();
+//init();
+promptQuestions();
